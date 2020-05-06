efb0aea24a56e7cbefb8cf7b5a7922e231be0d6e
Kevin Liang <kevinliang38@gmail.com>
2020-05-05 22:43:16 -0700

    added GET request

--- app/src/main/java/com/example/flashcard/MainActivityViewModel.kt
+++ app/src/main/java/com/example/flashcard/MainActivityViewModel.kt
@@ -5,11 +5,18 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
+import java.util.concurrent.CountDownLatch
+
 
 private const val TAG = "MainActivityViewModel"
 
 class MainActivityViewModel:ViewModel() {
-    private val booksLiveData: MutableLiveData<MutableList<Card>>
+        var cards = mutableListOf<Card>()
+        private val booksLiveData: MutableLiveData<MutableList<Card>>
+
 
     init{
         Log.i(TAG,"init")
@@ -20,9 +27,26 @@ class MainActivityViewModel:ViewModel() {
         return booksLiveData
     }
     private fun createCards():MutableList<Card> {
-        Log.i(TAG,"Create cards")
-        val cards = mutableListOf<Card>()
-        for (i in 1..150) cards.add(Card("Title #$i", "Content #$i"))
+        val url = "http://67.205.132.25/api/v1/resources/cards/all"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        val countDownLatch = CountDownLatch(1)
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                val gb = GsonBuilder().create()
+                cards = gb.fromJson(body, Array<Card>::class.java).toMutableList()
+                countDownLatch.countDown();
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println(e.toString())
+            }
+        })
+        //Log.i(TAG,"Create cards")
+        //for (i in 1..150) cards.add(Card("Title #$i", "Content #$i"))
+        countDownLatch.await()
         return cards
     }
 }
